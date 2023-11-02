555445e4bad68e8959d6f490b98fdb6f03f84b86
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 01:18:29 -0500

    Clase 1

--- src/main/java/udemy/poo/codigo/TiroParabolico.java
+++ src/main/java/udemy/poo/codigo/TiroParabolico.java
@@ -1,5 +1,40 @@
 package udemy.poo.codigo;
 
 public class TiroParabolico {
-    
+    private float velocidadInicial;
+    private int grado;
+    private final float gravedad = 9.8f;
+
+    public TiroParabolico(float velocidadInicial, int grado) {
+        this.velocidadInicial = velocidadInicial;
+        this.grado = grado;
+    }
+
+    public  float alturaMaxima(){
+
+      return 0.0f;
+    }
+    public float alcance(){
+      return 0.0f;
+    }
+
+  public float getVelocidadInicial() {
+    return velocidadInicial;
+  }
+
+  public void setVelocidadInicial(float velocidadInicial) {
+    this.velocidadInicial = velocidadInicial;
+  }
+
+  public int getGrado() {
+    return grado;
+  }
+
+  public void setGrado(int grado) {
+    this.grado = grado;
+  }
+
+  public float getGravedad() {
+    return gravedad;
+  }
 }
