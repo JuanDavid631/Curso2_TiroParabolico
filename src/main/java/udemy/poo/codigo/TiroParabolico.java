4559d2b12e9612521c6346e6db21755ca715ec06
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 01:23:30 -0500

    Clase 1

--- src/main/java/udemy/poo/codigo/TiroParabolico.java
+++ src/main/java/udemy/poo/codigo/TiroParabolico.java
@@ -11,11 +11,16 @@ public class TiroParabolico {
     }
 
     public  float alturaMaxima(){
-
-      return 0.0f;
+      float v0y = (float) (this.velocidadInicial * Math.sin(Math.toRadians(grado)));
+      float tiempo = v0y / this.gravedad;
+      float y = (float) (v0y * tiempo - (4.9f * Math.pow(tiempo, 2)));
+      return y;
     }
     public float alcance(){
-      return 0.0f;
+      float v0x = (float) (this.velocidadInicial * Math.cos(Math.toRadians(grado)));
+      float tiempo = (float) (2 * ((this.velocidadInicial * Math.sin(Math.toRadians(grado))) / this.gravedad));
+      float x = (v0x * tiempo);
+      return x;
     }
 
   public float getVelocidadInicial() {
