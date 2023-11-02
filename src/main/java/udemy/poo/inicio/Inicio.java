a0d2f571f94344b6c6db07ea2ed6cc1782003a80
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 01:28:53 -0500

    Clase 2

--- src/main/java/udemy/poo/inicio/Inicio.java
+++ src/main/java/udemy/poo/inicio/Inicio.java
@@ -1,5 +1,16 @@
 package udemy.poo.inicio;
 
+import udemy.poo.codigo.TiroParabolico;
+
+import javax.swing.*;
+
 public class Inicio {
-    
+  public static void main(String[] args) {
+    String dato = JOptionPane.showInputDialog(null, "Dame la velocidad inicial: "),
+            datoDos = JOptionPane.showInputDialog(null, "Dame el grado: ");
+    float numUno = Float.parseFloat(dato);
+    int numDos = Integer.parseInt(datoDos);
+
+    TiroParabolico objetoUno = new TiroParabolico(numUno, numDos);
+  }
 }
